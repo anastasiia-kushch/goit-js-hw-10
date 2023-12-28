0cd40b2c15c0002e5881bcf96666d79335ccb5d1
anastasiia-kushch <139702506+anastasiia-kushch@users.noreply.github.com>
2023-12-28 21:23:05 +0000

    Deploying to gh-pages from @ anastasiia-kushch/goit-js-hw-10@561439d53182c45feebf2887ed3eb85ba026cab5 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{f as t}from"./assets/vendor-992cd48f.js";const o=document.querySelector("#datetime-picker"),n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t(o,n);
+//# sourceMappingURL=commonHelpers.js.map
