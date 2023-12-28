561439d53182c45feebf2887ed3eb85ba026cab5
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2023-12-28 22:22:45 +0100

    first commit (basic)

---
+++ src/js/1-timer.js
@@ -0,0 +1,16 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputEl = document.querySelector('#datetime-picker');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(inputEl, options);
